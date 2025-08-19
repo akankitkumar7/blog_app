952ca4cd44221c63045c244951bf265e97a4ef8b
akankitkumar7 <22bec023@nith.ac.in>
2025-08-19 17:14:08 +0530

    initial commit

---
+++ lib/features/blog/presentation/bloc/blog_bloc.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:flutter_bloc/flutter_bloc.dart';
+part 'blog_event.dart';
+part 'blog_state.dart';
+
+class BlogBloc extends Bloc<BlogEvent, BlogState>{
+  BlogBloc() : super(BlogInitial()){
+    on<BlogEvent>((event, emit) => emit(BlogLoading()));
+    on<BlogUpload>(_onBlogUpload);
+  }
+
+  void _onBlogUpload(BlogUpload event, Emitter<BlogState> emit){
+
+  }
+
+
+}
